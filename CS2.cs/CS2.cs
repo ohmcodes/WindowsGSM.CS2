7ca52e791a842fba33c07e625cf48c87aaaa99ad
Ohmcodes <ohmcodes@gmail.com>
2023-11-18 12:37:28 -0800

    partial

---
+++ CS2.cs/CS2.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Engine;
+using WindowsGSM.GameServer.Query;
+using System.Collections.Generic;
+
+namespace WindowsGSM.Plugins
+{
+    public class CS2 : SteamCMDAgent
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.CS2", // WindowsGSM.XXXX
+            author = "ohmcodes",
+            description = "WindowsGSM plugin for supporting Counter Strike 2 Dedicated Server",
+            version = "1.0",
+            url = "https://github.com/ohmcodes/WindowsGSM.CS2", // Github repository link (Best practice)
+            color = "#FFA500" // Color Hex
+        };
+
+        // - Standard Constructor and properties
+        public CS2(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData;
+        public string Error, Notice;
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => true;
+        public override string AppId => "730"; /* taken via https://steamdb.info/app/730/info/ */
+
+        // - Game server Fixed variables
+        public override string StartPath => "game\\bin\\win64\\cs2.exe"; // Game server start path
+        public string FullName = "Counter Strike 2 Dedicated Server"; // Game server FullName
+        public bool AllowsEmbedConsole = true;  // Does this server support output redirect?
+        public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
+        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+
+        // - Game server default values
+        public string ServerName = "wgsm_cs2_dedicated";
+        public string Defaultmap = "de_dust2"; // Original (MapName)
+        public string Maxplayers = "10"; // WGSM reads this as string but originally it is number or int (MaxPlayers)
+        public string Port = "27015"; // WGSM reads this as string but originally it is number or int
+        public string QueryPort = "27016"; // WGSM reads this as string but originally it is number or int (SteamQueryPort)
+        public string Additional = "+sv_logfile 1";
+
+
+        private Dictionary<string, string> configData = new Dictionary<string, string>();
+
+
+        // - Create a default cfg for the game server after installation
+        public async void CreateServerCFG()
+        {
+            // no config yet
+            //+servercfgfile server.cfg
+        }
+
+        // - Start server function, return its Process to WindowsGSM
+        public async Task<Process> Start()
+        {
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+            if (!File.Exists(shipExePath))
+            {
+                Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
+                return null;
+            }
+
+            string param = "-dedicated";
+
+            param += $" +hostname {_serverData.ServerName}";
+            param += $" +map {_serverData.ServerMap}";
+            param += $" -ip {_serverData.ServerIP}";
+            param += $" -port {_serverData.ServerPort}";
+            param += $" -maxplayers {_serverData.ServerMaxPlayer}";
+
+            param += $" { _serverData.ServerParam}";
+            
+
+            // Prepare Process
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
+                    FileName = shipExePath,
+                    Arguments = param.ToString(),
+                    WindowStyle = ProcessWindowStyle.Minimized,
+                    UseShellExecute = false
+                },
+                EnableRaisingEvents = true
+            };
+
+            // Set up Redirect Input and Output to WindowsGSM Console if EmbedConsole is on
+            if (AllowsEmbedConsole)
+            {
+                p.StartInfo.CreateNoWindow = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                var serverConsole = new ServerConsole(_serverData.ServerID);
+                p.OutputDataReceived += serverConsole.AddOutput;
+                p.ErrorDataReceived += serverConsole.AddOutput;
+            }
+
+            // Start Process
+            try
+            {
+                p.Start();
+                if (AllowsEmbedConsole)
+                {
+                    p.BeginOutputReadLine();
+                    p.BeginErrorReadLine();
+                }
+
+                return p;
+            }
+            catch (Exception e)
+            {
+                Error = e.Message;
+                return null; // return null if fail to start
+            }
+        }
+
+        // - Stop server function
+        public async Task Stop(Process p)
+        {
+            await Task.Run(() =>
+            {
+                Functions.ServerConsole.SendMessageToMainWindow(p.MainWindowHandle, "quit");
+            });
+            await Task.Delay(20000);
+        }
+
+        // - Update server function
+        public async Task<Process> Update(bool validate = false, string custom = null)
+        {
+            var (p, error) = await Installer.SteamCMD.UpdateEx(serverData.ServerID, AppId, validate, custom: custom, loginAnonymous: loginAnonymous);
+            Error = error;
+            await Task.Run(() => { p.WaitForExit(); });
+            return p;
+        }
+
+        public bool IsInstallValid()
+        {
+            return File.Exists(Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath));
+        }
+
+        public bool IsImportValid(string path)
+        {
+            string exePath = Path.Combine(path, "PackageInfo.bin");
+            Error = $"Invalid Path! Fail to find {Path.GetFileName(exePath)}";
+            return File.Exists(exePath);
+        }
+
+        public string GetLocalBuild()
+        {
+            var steamCMD = new Installer.SteamCMD();
+            return steamCMD.GetLocalBuild(_serverData.ServerID, AppId);
+        }
+
+        public async Task<string> GetRemoteBuild()
+        {
+            var steamCMD = new Installer.SteamCMD();
+            return await steamCMD.GetRemoteBuild(AppId);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # WindowsGSM.CS2
 🧩 WindowsGSM plugin that provides Counter Strike 2 Dedicated server support!
+
+
+# WindowsGSM Installation: 
+1. Download  WindowsGSM https://windowsgsm.com/ 
+2. Create a Folder at a Location you wan't all Server to be Installed and Run.
+4. Drag WindowsGSM.Exe into previoulsy created folder and execute it.
+
+# Plugin Installation:
+1. Download [latest](https://github.com/ohmcodes/WindowsGSM.CS2/releases/latest) release
+2. Extract then Move **CS2.cs** folder to **plugins** folder
+3. OR Press on the Puzzle Icon in the left bottom side and install this plugin by navigating to it and select the Zip File.
+4. Click **[RELOAD PLUGINS]** button or restart WindowsGSM
+5. Navigate "Servers" and Click "Install Game Server" and find "Counter Strike 2 Dedicated Server [CS2.cs]
+
+### Official Documentation
+🗃️ https://developer.valvesoftware.com/wiki/Counter-Strike_2/Dedicated_Servers
+
+### The Game
+🕹️ https://store.steampowered.com/app/730/CounterStrike_2/
+
+### Dedicated server info
+🖥️ https://steamdb.info/app/730/info/
+
+# Automatic parameters via WindowsGSM.cfg
+### To navigate this click the Edit config button on selected server
+```
+hostname => Server Name
+map => Server Start Map
+port => Server Port
+maxplayers => Server Maxplayer
+```
+
+# Available parameters
+```
+-insecure	Allows VAC to be disabled.
++sv_logfile 1	Log server information in the log file.
++sv_lan 0	Server is a lan server ( no heartbeat, no authentication, no non-class C addresses ).
++game_alias deathmatch	Set the configuration of game type and mode based on game alias like 'deathmatch'.
++game_type 2 See Settings below.
++game_mode 0 See Settings below.
+```
+
+# Set Gamemodes
+### add param +game_alias competitive
+```
+Competitive:
+game_alias competitive <- sets both game mode and game type commands
+game_mode 1
+game_type 0
+
+Wingman
+game_alias wingman <- sets both game mode and game type commands
+game_mode 2
+game_type 0
+
+Casual
+game_alias casual <- sets both game mode and game type commands
+game_mode 0
+game_type 0
+
+Deathmatch
+game_alias deathmatch <- sets both game mode and game type commands
+game_mode 2
+game_type 1
+
+Custom
+game_alias custom <- sets both game mode and game type commands
+game_mode 0
+game_type 3
+```
+
+# Registering Game Server Login Token (GSLT)
+#### To navigate this on WGSM click Edit config button and you will see Server GSLT field
+
+1. Generate a token for your game server [View](http://steamcommunity.com/dev/managegameservers) 
+2. App ID of the base game (e.g. 440 for TF2, 730 for CS:GO)
+3. Memo (text stored with the account, just shown here to help you keep track)
+4. Paste the Login Token on your WGSM GSLT text field.
+
+
+# Set up cfg
+Directory ``` \game\csgo\cfg\server.cfg ```
+
+# License
+This project is licensed under the MIT License - see the <a href="https://github.com/ohmcodes/WindowsGSM.CS2/blob/main/LICENSE">LICENSE.md</a> file for details
