4394933d9e56a01536619524ef59cea03223d579
Stimayk <51941742+Stimayk@users.noreply.github.com>
2024-06-05 20:41:18 +0400

    Update CS2.cs

--- CS2.cs/CS2.cs
+++ CS2.cs/CS2.cs
@@ -1,65 +1,56 @@
-﻿using System;
-using System.IO;
+using System;
 using System.Diagnostics;
-using System.Text;
-using System.Net;
+using System.IO;
 using System.Threading.Tasks;
 using WindowsGSM.Functions;
 using WindowsGSM.GameServer.Engine;
 using WindowsGSM.GameServer.Query;
-using System.Collections.Generic;
 
 namespace WindowsGSM.Plugins
 {
     public class CS2 : SteamCMDAgent
     {
-        // - Plugin Details
+        // Plugin Details
         public Plugin Plugin = new Plugin
         {
-            name = "WindowsGSM.CS2", // WindowsGSM.XXXX
+            name = "WindowsGSM.CS2",
             author = "ohmcodes",
             description = "WindowsGSM plugin for supporting Counter Strike 2 Dedicated Server",
             version = "2.1.0",
-            url = "https://github.com/ohmcodes/WindowsGSM.CS2", // Github repository link (Best practice)
-            color = "#FFA500" // Color Hex
+            url = "https://github.com/ohmcodes/WindowsGSM.CS2",
+            color = "#FFA500"
         };
 
-        // - Standard Constructor and properties
-        public CS2(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        // Constructor
+        public CS2(ServerConfig serverData) : base(serverData) => _serverData = serverData;
         private readonly ServerConfig _serverData;
-        public string Error, Notice;
+        public new string Error, Notice;
 
-        // - Settings properties for SteamCMD installer
+        // SteamCMD installer settings
         public override bool loginAnonymous => true;
-        public override string AppId => "730"; /* taken via https://steamdb.info/app/730/info/ */
+        public override string AppId => "730";
 
-        // - Game server Fixed variables
-        public override string StartPath => "game\\bin\\win64\\cs2.exe"; // Game server start path
-        public string FullName = "Counter Strike 2 Dedicated Server"; // Game server FullName
-        public bool AllowsEmbedConsole = false;  // Does this server support output redirect?
-        public int PortIncrements = 1; // This tells WindowsGSM how many ports should skip after installation
-        public object QueryMethod = new A2S(); // Query method should be use on current server type. Accepted value: null or new A2S() or new FIVEM() or new UT3()
+        // Game server fixed variables
+        public override string StartPath => "game\\bin\\win64\\cs2.exe";
+        public string FullName = "Counter Strike 2 Dedicated Server";
+        public bool AllowsEmbedConsole = false;
+        public int PortIncrements = 1;
+        public object QueryMethod = new A2S();
 
-        // - Game server default values
+        // Game server default values
         public string ServerName = "wgsm_cs2_dedicated";
-        public string Defaultmap = "de_dust2"; // Original (MapName)
-        public string Maxplayers = "10"; // WGSM reads this as string but originally it is number or int (MaxPlayers)
-        public string Port = "27015"; // WGSM reads this as string but originally it is number or int
-        public string QueryPort = "27016"; // WGSM reads this as string but originally it is number or int (SteamQueryPort)
+        public string Defaultmap = "de_mirage";
+        public string Maxplayers = "10";
+        public string Port = "27015";
+        public string QueryPort = "27016";
         public string Additional = "+sv_logfile 1";
 
+        private Process _serverProcess;
 
-        private Dictionary<string, string> configData = new Dictionary<string, string>();
+        // Create a default cfg for the game server after installation
+        public async void CreateServerCFG() { }
 
-
-        // - Create a default cfg for the game server after installation
-        public async void CreateServerCFG()
-        {
-            // no config yet
-            //+servercfgfile server.cfg
-        }
-
-        // - Start server function, return its Process to WindowsGSM
+        // Start server function, return its Process to WindowsGSM
         public async Task<Process> Start()
         {
             string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
@@ -69,76 +60,101 @@ namespace WindowsGSM.Plugins
                 return null;
             }
 
-            string param = "-dedicated";
+            string param = $"-dedicated +hostname {_serverData.ServerName} +map {_serverData.ServerMap} -ip {_serverData.ServerIP} -port {_serverData.ServerPort} -maxplayers {_serverData.ServerMaxPlayer} {_serverData.ServerParam}";
 
-            param += $" +hostname {_serverData.ServerName}";
-            param += $" +map {_serverData.ServerMap}";
-            param += $" -ip {_serverData.ServerIP}";
-            param += $" -port {_serverData.ServerPort}";
-            param += $" -maxplayers {_serverData.ServerMaxPlayer}";
-
-            param += $" {_serverData.ServerParam}";
-            
-
-            // Prepare Process
-            var p = new Process
+            _serverProcess = new Process
             {
-                StartInfo =
+                StartInfo = new ProcessStartInfo
                 {
                     WorkingDirectory = ServerPath.GetServersServerFiles(_serverData.ServerID),
                     FileName = shipExePath,
-                    Arguments = param.ToString(),
+                    Arguments = param,
                     WindowStyle = ProcessWindowStyle.Normal,
-                    UseShellExecute = false
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true
                 },
                 EnableRaisingEvents = true
             };
 
-            // Start Process
             try
             {
-                p.Start();
-
-                return p;
+                _serverProcess.OutputDataReceived += ServerOutputDataReceived;
+                _serverProcess.ErrorDataReceived += ServerOutputDataReceived;
+                _serverProcess.Start();
+                _serverProcess.BeginOutputReadLine();
+                _serverProcess.BeginErrorReadLine();
+                return _serverProcess;
             }
             catch (Exception e)
             {
                 Error = e.Message;
-                return null; // return null if fail to start
+                return null;
             }
         }
-		
-        // - Update server function
-        public async Task<Process> Update(bool validate = false, string custom = null)
+
+        // Stop server function
+        public async Task Stop(Process p)
         {
-            var (p, error) = await Installer.SteamCMD.UpdateEx(serverData.ServerID, AppId, validate, custom: custom, loginAnonymous: loginAnonymous);
+            await Task.Run(() => ServerConsole.SendMessageToMainWindow(p.MainWindowHandle, "quit"));
+        }
+
+        // Restart server function
+        public async Task Restart(Process p)
+        {
+            await Stop(p);
+            await Start();
+        }
+
+        // Update server function
+        public new async Task<Process> Update(bool validate = false, string custom = null)
+        {
+            var (p, error) = await Installer.SteamCMD.UpdateEx(_serverData.ServerID, AppId, validate, custom: custom, loginAnonymous: loginAnonymous);
             Error = error;
-            await Task.Run(() => { p.WaitForExit(); });
+            p.WaitForExit();
             return p;
         }
 
-        public bool IsInstallValid()
+        // Validate installation
+        public new bool IsInstallValid()
         {
             return File.Exists(Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath));
         }
 
-        public bool IsImportValid(string path)
+        // Validate import path
+        public new bool IsImportValid(string path)
         {
             string exePath = Path.Combine(path, "PackageInfo.bin");
-            Error = $"Invalid Path! Fail to find {Path.GetFileName(exePath)}";
-            return File.Exists(exePath);
+            if (!File.Exists(exePath))
+            {
+                Error = $"Invalid Path! Fail to find {Path.GetFileName(exePath)}";
+                return false;
+            }
+            return true;
         }
 
-        public string GetLocalBuild()
+        // Get local build
+        public new string GetLocalBuild()
         {
             var steamCMD = new Installer.SteamCMD();
             return steamCMD.GetLocalBuild(_serverData.ServerID, AppId);
         }
 
-        public async Task<string> GetRemoteBuild()
+        // Get remote build
+        public new async Task<string> GetRemoteBuild()
         {
             var steamCMD = new Installer.SteamCMD();
             return await steamCMD.GetRemoteBuild(AppId);
+        }
+
+        // Handle server output
+        private void ServerOutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                Console.WriteLine(e.Data);
+                MainWindow._serverMetadata[int.Parse(_serverData.ServerID)].ServerConsole.Add(e.Data);
+            }
         }
     }
 }
